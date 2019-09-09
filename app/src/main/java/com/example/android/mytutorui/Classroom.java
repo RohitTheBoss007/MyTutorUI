0b84ea22b7fa824a968cc4264d4b86f9b01a4259
Rohit Pal <rohitpal210@gmail.com>
2019-09-09 20:20:45 +0530

    classroom ui

---
+++ app/src/main/java/com/example/android/mytutorui/Classroom.java
@@ -0,0 +1,13 @@
+package com.example.android.mytutorui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Classroom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dxc);
+    }
+}
