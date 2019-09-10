69ff35e21af254a1b509341561a2526fbed1b845
Rohit Pal <rohitpal210@gmail.com>
2019-09-10 22:05:49 +0530

    classroom ui and item

---
+++ app/src/main/java/com/example/android/mytutorui/InspirationFragment.java
@@ -0,0 +1,29 @@
+package com.example.android.mytutorui;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class InspirationFragment extends Fragment {
+
+
+    public InspirationFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_inspiration, container, false);
+    }
+
+}
