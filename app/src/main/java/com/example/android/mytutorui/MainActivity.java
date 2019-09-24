bb4a49a0cacb52dcfdc95cc9557b341aa7e0abd2
Rohit Pal <rohitpal210@gmail.com>
2019-09-24 22:00:45 +0530

    feed_item

--- app/src/main/java/com/example/android/mytutorui/MainActivity.java
+++ app/src/main/java/com/example/android/mytutorui/MainActivity.java
@@ -14,8 +14,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        startActivity(new Intent(MainActivity.this,ImageSliderActivity.class));
+        setContentView(R.layout.item_feed_main);
+        //startActivity(new Intent(MainActivity.this,ImageSliderActivity.class));
 
     }
 }
